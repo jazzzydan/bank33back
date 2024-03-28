857288162480436230348e30d5463c6f139a4a32
Rain <rain.tuur@gmail.com>
2024-03-28 16:06:11 +0200

    RAIN WIP

--- database/3_import.sql
+++ database/3_import.sql
@@ -1,18 +1,12 @@
-INSERT INTO public.role (id, name) VALUES (DEFAULT, 'admin');
-INSERT INTO public.role (id, name) VALUES (DEFAULT, 'customer');
-
-INSERT INTO public."user" (id, role_id, username, password, status) VALUES (DEFAULT, 1, 'admin', '123', 'A');
-INSERT INTO public."user" (id, role_id, username, password, status) VALUES (DEFAULT, 2, 'rain', '123', 'A');
-INSERT INTO public."user" (id, role_id, username, password, status) VALUES (DEFAULT, 2, 'mitteaktiivne', '123', 'D');
-
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Pärnu');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tallinn');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tartu');
+INSERT INTO public.role (id, name) VALUES (default, 'admin');
+INSERT INTO public.role (id, name) VALUES (default, 'customer');
 
 
-INSERT INTO public.location (id, city_id, name, number_of_atms, image_id, status) VALUES (DEFAULT, 2, 'Sikupilli Supermarket', 5, null, 'A');
-INSERT INTO public.location (id, city_id, name, number_of_atms, image_id, status) VALUES (DEFAULT, 3, 'Tondi Supermarket', 2, null, 'A');
+INSERT INTO public.city (id, name) VALUES (default, 'Tartu');
+INSERT INTO public.city (id, name) VALUES (default, 'Tallinn');
+INSERT INTO public.city (id, name) VALUES (default, 'Pärnu');
 
-INSERT INTO public.transaction_type (id, name) VALUES (DEFAULT, 'raha sisse');
-INSERT INTO public.transaction_type (id, name) VALUES (DEFAULT, 'raha välja');
-INSERT INTO public.transaction_type (id, name) VALUES (DEFAULT, 'maksed');
+
+INSERT INTO public.location (id, city_id, name, number_of_atms, status) VALUES (default, 2, 'Sikupilli Prisma', 5, 'A');
+INSERT INTO public.location (id, city_id, name, number_of_atms, status) VALUES (default, 2, 'Tondi Selver', 3, 'A');
+INSERT INTO public.location (id, city_id, name, number_of_atms, status) VALUES (default, 1, 'Sõbra Prisma', 2, 'A');
