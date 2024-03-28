e695b5b0f4472b43da7931fd298d4782632476d6
Rain <rain.tuur@gmail.com>
2024-03-28 16:12:44 +0200

    RAIN WIP

--- database/4_import_location_transaction_type.sql
+++
@@ -1,7 +0,0 @@
-
-
-INSERT INTO public.location_transaction_type (id, location_id, transaction_type_id ) VALUES (DEFAULT, 1, 1);
-INSERT INTO public.location_transaction_type (id, location_id, transaction_type_id ) VALUES (DEFAULT, 1, 2);
-INSERT INTO public.location_transaction_type (id, location_id, transaction_type_id ) VALUES (DEFAULT, 1, 3);
-INSERT INTO public.location_transaction_type (id, location_id, transaction_type_id ) VALUES (DEFAULT, 2, 1);
-INSERT INTO public.location_transaction_type (id, location_id, transaction_type_id ) VALUES (DEFAULT, 2, 2);
