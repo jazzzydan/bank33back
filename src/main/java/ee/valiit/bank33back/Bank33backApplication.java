ea6ffafd9ad1c0c304b21f062a283393be7dccfe
Rain <rain.tuur@gmail.com>
2024-03-28 12:47:42 +0200

    Intial commit

---
+++ src/main/java/ee/valiit/bank33back/Bank33backApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.bank33back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank33backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bank33backApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/bank33back/Bank33backApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.bank33back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bank33backApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
