004b4f5178f7d0094f060c9a7aedcf9b4b2e5307
Rain <rain.tuur@gmail.com>
2024-04-02 11:38:52 +0300

    RAIN-BEB-5 COMPLETE

--- src/main/java/ee/valiit/bank33back/business/LoginService.java
+++ src/main/java/ee/valiit/bank33back/business/LoginService.java
@@ -3,7 +3,6 @@ package ee.valiit.bank33back.business;
 import ee.valiit.bank33back.business.dto.LoginResponse;
 import ee.valiit.bank33back.domain.user.User;
 import ee.valiit.bank33back.domain.user.UserRepository;
-import ee.valiit.bank33back.infrastructure.exception.ForbiddenException;
 import ee.valiit.bank33back.infrastructure.validation.ValidationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,6 +22,7 @@ public class LoginService {
 
         User user = ValidationService.getValidExistingUser(optionalUser);
 
+        // todo: Võta MapStruct mapper kasutusele
         LoginResponse loginResponse = new LoginResponse();
         loginResponse.setUserId(user.getId());
         loginResponse.setRoleName(user.getRole().getName());
