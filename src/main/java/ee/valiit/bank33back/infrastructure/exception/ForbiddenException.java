a65c6fcb8ad9d19e8a58ed2c29f0bd93aa1f8682
Rain <rain.tuur@gmail.com>
2024-04-02 09:54:19 +0300

    RAIN-BEB-5 lisasin infrastructure osa

---
+++ src/main/java/ee/valiit/bank33back/infrastructure/exception/ForbiddenException.java
@@ -0,0 +1,15 @@
+package ee.valiit.bank33back.infrastructure.exception;
+
+import lombok.Data;
+
+@Data
+public class ForbiddenException extends RuntimeException {
+    private final String message;
+    private final Integer errorCode;
+
+    public ForbiddenException(String message, Integer errorCode) {
+        super(message);
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+}
