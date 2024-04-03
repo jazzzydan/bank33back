dd953ba43212a6599d9be4cab2857e54627bf61d
Rain <rain.tuur@gmail.com>
2024-04-03 12:33:33 +0300

    RAIN-BEB-8 WIP

--- src/main/java/ee/valiit/bank33back/business/location/LocationService.java
+++ src/main/java/ee/valiit/bank33back/business/location/LocationService.java
@@ -4,6 +4,8 @@ import ee.valiit.bank33back.business.location.dto.LocationInfo;
 import ee.valiit.bank33back.domain.location.Location;
 import ee.valiit.bank33back.domain.location.LocationMapper;
 import ee.valiit.bank33back.domain.location.LocationRepository;
+import ee.valiit.bank33back.domain.transaction.locationtransactiontype.LocationTransactionTypeRepository;
+import ee.valiit.bank33back.domain.transaction.transactiontype.TransactionType;
 import ee.valiit.bank33back.infrastructure.validation.ValidationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,18 @@ public class LocationService {
 
     private LocationRepository locationRepository;
     private LocationMapper locationMapper;
+    private LocationTransactionTypeRepository locationTransactionTypeRepository;
 
     public List<LocationInfo> findAtmLocations(Integer cityId) {
         List<Location> locations = locationRepository.findLocationsBy(cityId);
         ValidationService.validateLocationExists(locations);
 
         List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
+
+        for (LocationInfo locationInfo : locationInfos) {
+            List<TransactionType> transactionTypes = locationTransactionTypeRepository.findTransactionTypesBy(locationInfo.getLocationId());
+
+        }
 
 
         return locationInfos;
--- src/main/java/ee/valiit/bank33back/domain/transaction/locationtransactiontype/LocationTransactionTypeRepository.java
+++ src/main/java/ee/valiit/bank33back/domain/transaction/locationtransactiontype/LocationTransactionTypeRepository.java
@@ -1,9 +1,14 @@
 package ee.valiit.bank33back.domain.transaction.locationtransactiontype;
 
+import ee.valiit.bank33back.domain.transaction.transactiontype.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LocationTransactionTypeRepository extends JpaRepository<LocationTransactionType, Integer> {
-
+    @Query("select l.transactionType from LocationTransactionType l where l.location.id = :locationId order by l.transactionType.id")
+    List<TransactionType> findTransactionTypesBy(Integer locationId);
 
 
 }
