288f0de6312ad2cfd11cc71606e60193f6f8f47f
Rain <rain.tuur@gmail.com>
2024-04-04 14:49:09 +0300

    RAIN-BEB-9 WIP

--- src/main/java/ee/valiit/bank33back/domain/location/LocationRepository.java
+++ src/main/java/ee/valiit/bank33back/domain/location/LocationRepository.java
@@ -3,6 +3,7 @@ package ee.valiit.bank33back.domain.location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -10,8 +11,8 @@ public interface LocationRepository extends JpaRepository<Location, Integer> {
     @Query("select l from Location l where (l.city.id = :cityId or 0 = :cityId) order by l.city.name, l.name")
     List<Location> findLocationsBy(Integer cityId);
 
-    @Query("select (count(l) > 0) from Location l where upper(l.name) = upper(:name)")
-    boolean locationNameExists(String locationName);
+    @Query("select (count(l) > 0) from Location l where upper(l.name) = upper(:locationName)")
+    Boolean locationNameExists(String locationName);
 
 
 }
