57ec4ce28bbf94c93d37fd69e51e146b53ec6f25
Rain <rain.tuur@gmail.com>
2024-04-04 10:09:17 +0300

    RAIN-BEB-9 WIP

---
+++ src/main/java/ee/valiit/bank33back/domain/location/locationimage/LocationImageRepository.java
@@ -0,0 +1,6 @@
+package ee.valiit.bank33back.domain.location.locationimage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationImageRepository extends JpaRepository<LocationImage, Integer> {
+}
