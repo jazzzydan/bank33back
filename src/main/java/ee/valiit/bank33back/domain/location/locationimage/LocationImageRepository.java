03217704c89236c6b15ac262e93ee578cec7dcbe
Daniil Starov <jazzzydan@hotmail.com>
2024-04-05 10:59:19 +0300

    BEB-10 Finished

--- src/main/java/ee/valiit/bank33back/domain/location/locationimage/LocationImageRepository.java
+++ src/main/java/ee/valiit/bank33back/domain/location/locationimage/LocationImageRepository.java
@@ -2,7 +2,6 @@ package ee.valiit.bank33back.domain.location.locationimage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
